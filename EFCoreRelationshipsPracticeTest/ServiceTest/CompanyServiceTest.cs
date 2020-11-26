54fd8ffb508b96855d119da245a8928bc77f9e0a
Xiaoyu Lu <xlu14@slb.com>
2020-11-26 14:47:23 +0800

    feat: update migration

---
+++ EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreRelationshipsPracticeTest.ServiceTest
+{
+    public class CompanyServiceTest
+    {
+        public CompanyServiceTest()
+        {
+        }
+    }
+}
